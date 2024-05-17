1f4929ef453a58560c11386300c4a4dfccfcff13
Vishalk1999 <kalevishalm97@gmail.com>
2024-05-17 09:28:16 +0530

    Added all

---
+++ file1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class File1
+{
+  public static void main(string args[])
+  {
+System.out.println("This is first file");
+   System.out.println("This is second file");
+
+  }
+}
